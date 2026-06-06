b98f485395aa689db15ada3357cd67c3517eaf8e
shiv <shivpatel.cv@gmail.com>
2026-06-06 22:13:53 +0530

    feat: initialize Spring Boot backend skeleton

---
+++ src/main/java/com/shiv/securegkd/SecureGkdBackendApplication.java
@@ -0,0 +1,13 @@
+package com.shiv.securegkd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecureGkdBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecureGkdBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shiv/securegkd/SecureGkdBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shiv.securegkd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecureGkdBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
