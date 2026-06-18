494c6cdc6f39a552048ac023f4c9eb09b54db7d9
shiv <shivpatel.cv@gmail.com>
2026-06-18 21:58:43 +0530

    feat: add Game entity and repository

---
+++ src/main/java/com/shiv/securegkd/game/GameRepository.java
@@ -0,0 +1,12 @@
+package com.shiv.securegkd.game;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface GameRepository extends JpaRepository<Game, Long> {
+
+    Optional<Game> findByCode(String code);
+
+    boolean existsByCode(String code);
+}
